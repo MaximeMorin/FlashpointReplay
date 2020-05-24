898ac291cba13454ca80a06ffe32c2e1d523db5e
Maxime "MadMax" Morin <maxime.m.morin@gmail.com>
2020-05-24 15:57:21 -0400

    First working tentative - testing in progress.

---
+++ Rewin.BattleTech.FlashpointReplay/Runner.cs
@@ -0,0 +1,15 @@
+﻿using Harmony;
+using System.Reflection;
+
+namespace Rewin.BattleTech.FlashpointReplay
+{
+    public static class Runner
+    {
+
+        public static void Init()
+        {
+            var harmony = HarmonyInstance.Create("ca.rewin.battletech.flashpoint-replay");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
