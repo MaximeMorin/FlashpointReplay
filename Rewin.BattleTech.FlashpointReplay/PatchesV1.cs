ededd55475d75e3bf370781c31bcded1fb06f566
Maxime "MadMax" Morin <maxime.m.morin@gmail.com>
2020-05-30 14:13:26 -0400

    Switch to a different strategy.
    Instead of trying to keep the mod on and always flush flashpoint data, the new stragety is to turn the mod on to flush and then turn it off.

---
+++ Rewin.BattleTech.FlashpointReplay/PatchesV1.cs
@@ -0,0 +1,98 @@
+﻿using BattleTech;
+using Harmony;
+using HBS.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace Rewin.BattleTech.FlashpointReplay
+{
+    [HarmonyPatch(typeof(SimGameState), "CompleteFlashpoint")]
+    public static class SimGameState_CompleteFlashpoint_Patch
+    {
+        public static void Postfix(SimGameState __instance)
+        {
+            __instance.completedFlashpoints = new List<string>();
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "Rehydrate")]
+    public static class SimGameState_Rehydrate_Patch
+    {
+        public static void Postfix(SimGameState __instance)
+        {
+            __instance.completedFlashpoints = new List<string>();
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "InitializeDataFromDefs")]
+    public static class SimGameState_InitializeDataFromDefs_Patch
+    {
+        public static void Postfix(ref List<FlashpointDef> ___initialFlashpointPool)
+        {
+            ___initialFlashpointPool.Clear();
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsRequirements", new Type[] { typeof(RequirementDef[]) })]
+    public static class SimGameState_MeetsRequirements_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsRequirements", new Type[] { typeof(RequirementDef), typeof(SimGameReport.ReportEntry) })]
+    public static class SimGameState_MeetsRequirements_WithLogs_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsRequirements", new Type[] { typeof(TagSet), typeof(TagSet), typeof(List<ComparisonDef>), typeof(TagSet), typeof(StatCollection), typeof(SimGameReport.ReportEntry)})]
+    public static class SimGameState_MeetsRequirements_WithTagSet_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsRequirements", new Type[] { typeof(RequirementDef), typeof(TagSet), typeof(StatCollection), typeof(SimGameReport.ReportEntry)})]
+    public static class SimGameState_MeetsRequirements_WithTagSetStats_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsStatRequirements")]
+    public static class SimGameState_MeetsStatRequirements_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "MeetsTagRequirements")]
+    public static class SimGameState_MeetsTagRequirements_Patch
+    {
+        public static void Postfix(ref bool __result)
+        {
+            __result = true;
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "CheckForNewFlashpoints")]
+    public static class SimGameState__CheckForNewFlashpoints_Patch
+    {
+        public static void Prefix(ref int ___MaxGenFlashpointsPerDay)
+        {
+            ___MaxGenFlashpointsPerDay = 10;
+        }
+    }
+}
---
+++ Rewin.BattleTech.FlashpointReplay/PatchesV2.cs
@@ -0,0 +1,62 @@
+﻿using BattleTech;
+using Harmony;
+using HBS.Collections;
+using System;
+using System.Collections.Generic;
+
+namespace Rewin.BattleTech.FlashpointReplay
+{
+
+    [HarmonyPatch(typeof(SimGameState), "Rehydrate")]
+    public static class SimGameState_Rehydrate_Patch
+    {
+        public static void Postfix(SimGameState __instance,
+            ref List<SimGameMilestoneDef> ___milestones,
+            ref List<string> ___ConsumedMilestones,
+            ref List<string> ___contractDiscardPile,
+            ref List<string> ___mapDiscardPile,
+            ref List<string> ___ignoredContractEmployers,
+            ref List<string> ___ignoredContractTargets,
+            ref List<string> ___availableFlashpointList,
+            ref Flashpoint ___activeFlashpoint,
+            ref bool ___inFlashpointCooldown,
+            ref int ___flashpointCooldownDays,
+            ref bool ___needQueuedMilestoneCheck)
+        {
+            __instance.completedFlashpoints.Clear();
+            __instance.flashpointDiscardPile.Clear();
+            __instance.AlreadyClickedConversationResponses.Clear();
+
+            ___milestones.Clear();
+            ___ConsumedMilestones.Clear();
+            ___contractDiscardPile.Clear();
+            ___mapDiscardPile.Clear();
+            ___ignoredContractEmployers.Clear();
+            ___ignoredContractTargets.Clear();
+            ___availableFlashpointList.Clear();
+            ___activeFlashpoint = null;
+            ___inFlashpointCooldown = false;
+            ___flashpointCooldownDays = 0;
+            ___needQueuedMilestoneCheck = false;            
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "InitializeDataFromDefs")]
+    public static class SimGameState_InitializeDataFromDefs_Patch
+    {
+        public static void Postfix(ref List<FlashpointDef> ___initialFlashpointPool)
+        {
+            ___initialFlashpointPool.Clear();
+        }
+    }
+
+    [HarmonyPatch(typeof(SimGameState), "CheckForNewFlashpoints")]
+    public static class SimGameState__CheckForNewFlashpoints_Patch
+    {
+        public static void Prefix(ref int ___MaxGenFlashpointsPerDay, ref int ___MaxActiveFlashpoints)
+        {
+            ___MaxGenFlashpointsPerDay = 10;
+            ___MaxActiveFlashpoints = 20;
+        }
+    }
+}
